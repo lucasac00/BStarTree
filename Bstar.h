22595236b1d5fc6faf1f433537e8b393c1925718
Arthur Fonseca <arthurbfonseca27@gmail.com>
2023-08-01 13:42:42 -0300

    Fix/ divisão de tarefas

--- Bstar.h
+++ Bstar.h
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Ordem 4
-
 typedef struct Noh
 {
-    int chaves[Ordem - 1];
-    struct Noh *filhos[Ordem];
+    int *chaves;
+    struct Noh **filhos;
     struct Noh *pai;
     int n;       // Conta quantos dos espaços de chave estão preenchidos
     int ehfolha; // Informa se o nó é um nó folha. 1 = é folha, 0 = não é folha
@@ -18,30 +16,30 @@ typedef struct BStarTree
     int ordem;
 } BStarTree;
 
-BStarTree *inicializaArvore(int ordem);
+BStarTree *inicializaArvore(int ORDEM); //Arthur
 
-BStarTree *inicializaNoh(int ordem);
+BStarTree *inicializaNoh(int ORDEM); // Arthur
 
 // Função de split usada quando há overflow em nós
-void split2in3(BStarTree *arvore, Noh *noh, int index);
+void split2in3(BStarTree *arvore, Noh *noh, int index); //Arthur 
 
 // Função para fazer o merge de nós em caso de underflow
-void mergeNoh(BStarTree *arvore, Noh *pai, int index);
+void mergeNoh(BStarTree *arvore, Noh *pai, int index); //Arthur
 
 // Função caller da busca
-BStarTree *busca(BStarTree *arvore, int chave);
+BStarTree *busca(BStarTree *arvore, int chave); //Lucas
 
-BStarTree *_busca(BStarTree *arvore, Noh *noh, int chave);
+BStarTree *_busca(BStarTree *arvore, Noh *noh, int chave); // Lucas
 
 // Função caller da inserção
-void insere(BStarTree *arvore, int chave);
+void insere(BStarTree *arvore, int chave); // Lucas
 
-void _insere(BStarTree *arvore, Noh *noh, int chave);
+void _insere(BStarTree *arvore, Noh *noh, int chave); // Lucas
 
 // Função caller da remoção
-void remove(BStarTree *arvore, int chave);
+void remove(BStarTree *arvore, int chave); // Luiz
 
-void _remove(BStarTree *arvore, Noh *noh, int chave);
+void _remove(BStarTree *arvore, Noh *noh, int chave); // Luiz
 
 // Função para imprimir todas as chaves da árvore, inorder
-void print(BStarTree *arvore);
+void print(BStarTree *arvore); // Luiz
